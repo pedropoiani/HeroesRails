4baa6236bce911a4fa4e6b3ed698586832c1b6bf
Élisson Michael <elissonmichael@yahoo.com.br>
2019-10-09 20:00:56 -0300

    Resolve Erro em Create Poder

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- app/models/personagem.rb
+++ app/models/personagem.rb
@@ -1,4 +1,7 @@
 class Personagem < ApplicationRecord
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" },
+                            default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates_presence_of :nome, :forca, :resistencia, :agilidade, :destreza
   has_many :poderes
 
